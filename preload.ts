b3f7ddd9ac7e9ea32e922049f83c62b62ac539c7
Iwo Plaza <iwoplaza@gmail.com>
2025-08-04 20:04:05 +0200

    Cleanup

---
+++ preload.ts
@@ -0,0 +1,8 @@
+import { plugin } from "bun"
+import typegpu from "unplugin-typegpu/bun"
+
+plugin(
+  typegpu({
+    include: /\.ts$/,
+  }),
+)
--- typegpu-plugin.ts
+++
@@ -1,8 +0,0 @@
-import { plugin } from "bun"
-import { bunPlugin } from "unplugin-typegpu"
-
-plugin(
-  bunPlugin({
-    include: /\.m?[t]sx?$/,
-  }),
-)
