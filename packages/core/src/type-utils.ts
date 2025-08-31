661f34ec86fe9089d380d60eb7b9cd913ff46259
Gokhan Kurt <krtgkn@gmail.com>
2025-09-01 00:29:22 +0300

    remove type-utils

--- packages/core/src/type-utils.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Represents a timeout handle.
- * Added here to avoid platform specific type as each one has a different API (Bun, Node, DOM)
- */
-export type Timeout = ReturnType<typeof setTimeout> | undefined
