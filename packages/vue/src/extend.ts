63dd1d38cb8110630369505b8dbce1fde33d0541
Bhushan <bhushanwagh6@gmail.com>
2025-09-02 21:16:28 +0530

    adds : extend feature

---
+++ packages/vue/src/extend.ts
@@ -0,0 +1,7 @@
+
+import { elements } from './elements'
+import type { OpenTUIComponents } from '../types/opentui'
+
+export function extend<T extends Partial<OpenTUIComponents>>(newElements: T) {
+  Object.assign(elements, newElements)
+}
