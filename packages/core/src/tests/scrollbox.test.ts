54473e2431fa0bc726f924a9acf69626751f8583
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2025-10-05 11:00:49 -0400

    Delegate scollbox insertBefore to content to fix Anchor not found error

--- packages/core/src/tests/scrollbox.test.ts
+++ packages/core/src/tests/scrollbox.test.ts
@@ -2,7 +2,6 @@ import { test, expect, beforeEach, afterEach, describe } from "bun:test"
 import { createTestRenderer, type TestRenderer } from "../testing/test-renderer"
 import { ScrollBoxRenderable } from "../renderables/ScrollBox"
 import { BoxRenderable } from "../renderables/Box"
-import { TextRenderable } from "../renderables/Text"
 
 let testRenderer: TestRenderer
 
@@ -12,6 +11,48 @@ beforeEach(async () => {
 
 afterEach(() => {
   testRenderer.destroy()
+})
+
+describe("ScrollBoxRenderable - child delegation", () => {
+  test("delegates add to content wrapper", () => {
+    const scrollbox = new ScrollBoxRenderable(testRenderer, { id: "scrollbox" })
+    const child = new BoxRenderable(testRenderer, { id: "child" })
+
+    scrollbox.add(child)
+
+    const children = scrollbox.getChildren()
+    expect(children.length).toBe(1)
+    expect(children[0].id).toBe("child")
+    expect(child.parent).toBe(scrollbox.content)
+  })
+
+  test("delegates remove to content wrapper", () => {
+    const scrollbox = new ScrollBoxRenderable(testRenderer, { id: "scrollbox" })
+    const child = new BoxRenderable(testRenderer, { id: "child" })
+
+    scrollbox.add(child)
+    expect(scrollbox.getChildren().length).toBe(1)
+
+    scrollbox.remove(child.id)
+    expect(scrollbox.getChildren().length).toBe(0)
+  })
+
+  test("delegates insertBefore to content wrapper", () => {
+    const scrollbox = new ScrollBoxRenderable(testRenderer, { id: "scrollbox" })
+    const child1 = new BoxRenderable(testRenderer, { id: "child1" })
+    const child2 = new BoxRenderable(testRenderer, { id: "child2" })
+    const child3 = new BoxRenderable(testRenderer, { id: "child3" })
+
+    scrollbox.add(child1)
+    scrollbox.add(child2)
+    scrollbox.insertBefore(child3, child2)
+
+    const children = scrollbox.getChildren()
+    expect(children.length).toBe(3)
+    expect(children[0].id).toBe("child1")
+    expect(children[1].id).toBe("child3")
+    expect(children[2].id).toBe("child2")
+  })
 })
 
 describe("ScrollBoxRenderable - destroyRecursively", () => {
