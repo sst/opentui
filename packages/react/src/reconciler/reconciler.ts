83d0b1153dddfa7390c14f79c76827d177b45c75
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-12-23 23:57:42 +0100

    fix: remove unused ts-expect-error from reconciler

--- packages/react/src/reconciler/reconciler.ts
+++ packages/react/src/reconciler/reconciler.ts
@@ -18,8 +18,6 @@ export function _render(element: React.ReactNode, root: RootRenderable) {
     "",
     console.error,
     console.error,
-    // @ts-expect-error the types for `react-reconciler` are not up to date with the library.
-    // See https://github.com/facebook/react/blob/7a36dfedc70ffb49be2e4e23b40e01d34cef267e/packages/react-reconciler/src/ReactFiberReconciler.js#L236-L259
     console.error,
     console.error,
     null,
