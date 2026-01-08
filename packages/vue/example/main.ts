63eb8d8450d32e84fbdb46191b7e22c243b999e2
Matt Simpson <7691252+msmps@users.noreply.github.com>
2026-01-08 15:02:58 +0000

    chore: remove unmaintained vue and go packages  (#494)

--- packages/vue/example/main.ts
+++
@@ -1,8 +0,0 @@
-import { render } from "@opentui/vue"
-import { extend } from "@opentui/vue"
-import { ConsoleButtonRenderable } from "./CustomButtonRenderable"
-import App from "./App.vue"
-
-extend({ consoleButtonRenderable: ConsoleButtonRenderable })
-
-render(App)
