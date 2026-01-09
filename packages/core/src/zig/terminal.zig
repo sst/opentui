8bbbb85baf440b0f13ffab23ffc29c851df10eb0
assagman <ahmetsercansagman@gmail.com>
2026-01-09 20:18:07 +0300

    chore: downgrade xtversion detection log from info to debug
    Signed-off-by: assagman <ahmetsercansagman@gmail.com>

--- packages/core/src/zig/terminal.zig
+++ packages/core/src/zig/terminal.zig
@@ -573,7 +573,7 @@ fn parseXtversion(self: *Terminal, term_str: []const u8) void {
 
     self.term_info.from_xtversion = true;
 
-    log.info("Terminal detected via xtversion: {s} {s}", .{
+    log.debug("Terminal detected via xtversion: {s} {s}", .{
         self.term_info.name[0..self.term_info.name_len],
         self.term_info.version[0..self.term_info.version_len],
     });
