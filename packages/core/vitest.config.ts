94b6651cff6059df2f0943c25d015ebd727ca93e
Jake Teton-Landis <just.1.jake@gmail.com>
2026-04-09 11:55:39 -0400

    vitest config

---
+++ packages/core/vitest.config.ts
@@ -0,0 +1,29 @@
+import { basename, dirname, join } from "node:path"
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    // globalSetup: "./src/nodejs/compat.ts",
+    environment: "node",
+    isolate: false,
+    // alias: {
+    //   "bun:ffi": "./src/nodejs/bunModules/ffi.ts",
+    //   "bun:test": "./src/nodejs/bunModules/test.ts",
+    // },
+    execArgv: [
+      // "--experimental-transform-types",
+      // "--import=tsx",
+      // "--import=@swc-node/register/esm",
+      "--no-experimental-strip-types",
+      "--experimental-transform-types",
+      // "--import=esbuild-register/loader",
+      "--import=./src/nodejs/compat.ts",
+    ],
+    experimental: {
+      viteModuleRunner: false,
+    },
+    // Create independent snapshots from bun:test
+    resolveSnapshotPath: (testPath, ext) =>
+      join(dirname(testPath), "__snapshots__", `${basename(testPath)}.nodejs.${ext}`),
+  },
+})
