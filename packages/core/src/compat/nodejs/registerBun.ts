047a00dc95ed87a95522870a3a1d9c4d6050d550
Jake Teton-Landis <just.1.jake@gmail.com>
2026-04-09 21:27:23 -0400

    improve chances an example works w/ registerBun

--- packages/core/src/compat/nodejs/registerBun.ts
+++ packages/core/src/compat/nodejs/registerBun.ts
@@ -1,6 +1,8 @@
 import * as mod from "node:module"
+import { extname } from "node:path"
 import { __url as ffiUrl } from "../ffi.js"
 import * as NodeBun from "../runtime.js"
+import { fileURLToPath } from "node:url";
 
 if (typeof globalThis.Bun === "undefined") {
   Object.defineProperty(globalThis, "Bun", {
@@ -11,16 +13,74 @@ if (typeof globalThis.Bun === "undefined") {
   })
 }
 
+const recentOddSpecifiers = new Map<string, mod.ResolveHookContext>()
+function popRecentSpecifier(specifier: string) {
+  const context = recentOddSpecifiers.get(specifier)
+  if (context) {
+    recentOddSpecifiers.delete(specifier)
+  } else if (recentOddSpecifiers.size > 10) {
+    const key = recentOddSpecifiers.keys().next().value
+    if (key) {
+      recentOddSpecifiers.delete(key)
+    }
+  }
+  return context
+}
+
+function extendError(error: unknown, specifier: string, context: mod.ResolveHookContext | undefined) {
+  if (error && typeof error === "object" && "message" in error) {
+    error.message += `\nSpecifier: '${specifier}'\nFrom: ${JSON.stringify(context, null, 2)}`
+  }
+  return error
+}
+
+const NORMAL_EXTENSIONS = new Set<string>([".js", ".mjs", ".cjs", ".jsx", ".ts", ".tsx"])
+
 mod.registerHooks({
   resolve: (specifier, context, next) => {
-    if (specifier === "bun:ffi") {
-      return next(ffiUrl, context)
-    }
+    try {
+      if (specifier === "bun:ffi") {
+        return next(ffiUrl, context)
+      }
 
-    if (specifier.startsWith("bun:")) {
-      throw new Error(`Untransformed Bun specifier: '${specifier}' from '${context.parentURL}'`)
-    }
+      if (specifier.startsWith("bun:")) {
+        throw new Error(`Untransformed Bun specifier: '${specifier}' from '${context.parentURL}'`)
+      }
 
-    return next(specifier, context)
+      const result = next(specifier, context)
+
+      if (
+        (!result.url.startsWith("node:") && !NORMAL_EXTENSIONS.has(extname(result.url))) ||
+        (context.importAttributes.type &&
+          context.importAttributes.type !== "module" &&
+          context.importAttributes.type !== "commonjs")
+      ) {
+        recentOddSpecifiers.set(result.url, context)
+      }
+
+      return result
+    } catch (error) {
+      throw extendError(error, specifier, context)
+    }
+  },
+  load: (specifier, context, next) => {
+    // Exists only for error reporting / debugging.
+    const resolveContext = popRecentSpecifier(specifier)
+    try {
+      return next(specifier, context)
+    } catch (error) {
+      if (context.importAttributes.type === "file" || context.importAttributes.type?.includes(
+        '/'
+      )) {
+        const absolutePath = fileURLToPath(specifier)
+        return {
+          format: 'module',
+          source: `export default ${JSON.stringify(absolutePath)}`,
+          shortCircuit: true,
+        }
+      }
+
+      throw extendError(error, specifier, resolveContext)
+    }
   },
 })
