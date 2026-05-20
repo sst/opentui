ef8f2ec764ff26abb6cdb594dfd6613edc7451ee
Sebastian Herrlinger <hasta84@gmail.com>
2026-05-20 15:08:23 +0200

    solid catalogue

--- packages/solid/components.ts
+++ packages/solid/components.ts
@@ -1,2 +1,2 @@
-export { extend, getComponentCatalogue } from "./src/elements/index.js"
+export { extend, getComponentCatalogue } from "./src/elements/catalogue.js"
 export type { ExtendedComponentProps, ExtendedIntrinsicElements, OpenTUIComponents } from "./src/types/elements.js"
