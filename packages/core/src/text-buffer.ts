db23a10a2d67d65c7c48daffeadd1091926a85af
Aviral Shukla <aviral.shukla2122000@gmail.com>
2026-06-17 11:38:56 +0530

    test fix

--- packages/core/src/text-buffer.ts
+++ packages/core/src/text-buffer.ts
@@ -30,13 +30,13 @@ export class TextBuffer {
   private _memId?: number
   private _appendedChunks: Uint8Array[] = []
 
-  constructor(lib: RenderLib, ptr: TextBufferHandle, options: TextBufferLinkOptions) {
+  constructor(lib: RenderLib, ptr: TextBufferHandle, options: TextBufferLinkOptions = {}) {
     this.lib = lib
     this.bufferPtr = ptr
     this.allowedLinkSchemes = options.allowedLinkSchemes
   }
 
-  static create(widthMethod: WidthMethod, options: TextBufferLinkOptions): TextBuffer {
+  static create(widthMethod: WidthMethod, options: TextBufferLinkOptions = {}): TextBuffer {
     const lib = resolveRenderLib()
     return lib.createTextBuffer(widthMethod, options)
   }
