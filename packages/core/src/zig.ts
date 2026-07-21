b2981e2a415a616571a960f1a730bc585e4288fd
Sebastian Herrlinger <hasta84@gmail.com>
2026-07-21 21:26:26 +0200

    normalize image ffi boolean

--- packages/core/src/zig.ts
+++ packages/core/src/zig.ts
@@ -3275,7 +3275,7 @@ class FFIRenderLib implements RenderLib {
       sourceHeight,
       protocol: protocolId,
     })
-    return this.opentui.symbols.bufferDrawImage(buffer, image, ptr(options))
+    return Boolean(this.opentui.symbols.bufferDrawImage(buffer, image, ptr(options)))
   }
 
   public bufferDrawPackedBuffer(
