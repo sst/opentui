218a11f135db9e091937f257b7cbd01a445562ca
Sebastian Herrlinger <hasta84@gmail.com>
2026-07-30 17:07:57 +0200

    fix(core): defer suspended setup queries

--- packages/core/src/renderer.ts
+++ packages/core/src/renderer.ts
@@ -3729,7 +3729,11 @@ export class CliRenderer extends EventEmitter implements RenderContext {
     }, this.resizeDebounceDelay)
   }
 
-  private queryPixelResolution() {
+  private queryPixelResolution(force: boolean = false) {
+    if (!force && this._controlState === RendererControlState.EXPLICIT_SUSPENDED) {
+      this.queryPixelResolutionOnResume = true
+      return
+    }
     this.pendingPixelResolutionQueries++
     if (this.pendingPixelResolutionQueries === 1) {
       this.updateStdinParserProtocolContext({ pixelResolutionQueryActive: true })
@@ -3762,11 +3766,7 @@ export class CliRenderer extends EventEmitter implements RenderContext {
 
     this._terminalWidth = width
     this._terminalHeight = height
-    if (this._controlState === RendererControlState.EXPLICIT_SUSPENDED) {
-      this.queryPixelResolutionOnResume = true
-    } else {
-      this.queryPixelResolution()
-    }
+    this.queryPixelResolution()
 
     this.setCapturedRenderable(undefined)
     this.stdinParser?.resetMouseState()
@@ -4092,7 +4092,7 @@ export class CliRenderer extends EventEmitter implements RenderContext {
 
     if (this.queryPixelResolutionOnResume) {
       this.queryPixelResolutionOnResume = false
-      this.queryPixelResolution()
+      this.queryPixelResolution(true)
     }
 
     this.suspendedNonAltSurfacePreserved = false
